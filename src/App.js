11c5366f05fc608458aafc170a95b7cd7905370b
volkangurbuz <volkangurbuz16@gmail.com>
2022-04-01 22:30:49 +0200

    using react context api

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from "./store/auth-context";
 
 function App() {
-  const  storedUserLoggedInfo= localStorage.getItem("isLoggedIn");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const storedUserLoggedInfo = localStorage.getItem("isLoggedIn");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useEffect(()=>{
-        if (storedUserLoggedInfo==='1'){
+    useEffect(() => {
+        if (storedUserLoggedInfo === '1') {
             setIsLoggedIn(true);
         }
-    }, [] )
+    }, [])
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-      //1 login 0 not logged, we store the data
-      localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        //1 login 0 not logged, we store the data
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
 
-  const logoutHandler = () => {
-      localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
-  };
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    };
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
-  );
+    //the new context object will be passed down to all listening(consuming) components
+    return (
+        <AuthContext.Provider value={{isLoggedIn: isLoggedIn,}}>
+            <MainHeader onLogout={logoutHandler}/>
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler}/>}
+                {isLoggedIn && <Home onLogout={logoutHandler}/>}
+            </main>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
---
+++ src/store/auth-context.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+//creates context object to use on another components
+const AuthContext = React.createContext({isLoggedIn: false});
+
+
+export default AuthContext;
